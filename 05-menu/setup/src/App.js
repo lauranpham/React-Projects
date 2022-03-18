60b75a2ab199a282d5106cb207358681e7c63f14
Laura Pham <l.pham2311@gmail.com>
2022-03-18 19:55:13 +1100

    react projects: menu project with hard coded categories

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,31 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = items.map((item) => item.category);
 function App() {
-  return <h2>menu project setup</h2>;
+	const [menuItems, setMenuItems] = useState(items);
+	const [categories, setCategories] = useState([]);
+
+	const filterItems = (category) => {
+		if (category === 'all') {
+			setMenuItems(items);
+			return;
+		}
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
+	return (
+		<main>
+			<section className='menu section'>
+				<div className='title'>
+					<h2>our menu</h2>
+					<div className='underline'></div>
+					<Categories filterItems={filterItems} />
+					<Menu items={menuItems} />
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
