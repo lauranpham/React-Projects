2dde23cbc2f8a67e1df78aaac3671ea331738807
Laura Pham <l.pham2311@gmail.com>
2022-03-23 20:30:11 +1100

    color generator: copy color to clipboard and setTimeout

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -4,17 +4,28 @@ import rgbToHex from './utils';
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
 	const [alert, setAlert] = useState(false);
 	const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb) // use spread operator to pass individual values to function
-	const hexValue = `#${hexColor}`
-  // set white color to colors greater than the base - at index 10.
-  return (
+	const hex = rgbToHex(...rgb); // use spread operator to pass individual values to function
+	const hexValue = `#${hexColor}`;
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setAlert(false);
+		}, 3000);
+		return () => clearTimeout(timeout); // clear previous timeout
+	}, [alert]);
+
+	return (
 		<article
-			className={`color  ${index > 10 && 'color-light'}`}
+			className={`color  ${index > 10 && 'color-light'}`} 	// set white color to colors greater than the base - at index 10.
 			style={{ backgroundColor: `rgb(${bcg})` }}
+			onClick={() => {
+				setAlert(true);
+				navigator.clipboard.writeText(hexValue); // write color text to clipboard for use
+			}}
 		>
-      <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexValue}</p>
-    </article>
+			<p className='percent-value'>{weight}%</p>
+			<p className='color-value'>{hexValue}</p>
+			{alert && <p className='alert'>copied to clipboard</p>}
+		</article>
 	);
 };
 
