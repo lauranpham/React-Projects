495f4756a34a9790f61a87ebefa5717dd35de9c9
Laura Pham <l.pham2311@gmail.com>
2022-03-23 20:23:26 +1100

    color generator: update text color for dark shade backgrounds

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
 	const [alert, setAlert] = useState(false);
 	const bcg = rgb.join(',');
   const hex = rgbToHex(...rgb) // use spread operator to pass individual values to function
-	return (
+	const hexValue = `#${hexColor}`
+  // set white color to colors greater than the base - at index 10.
+  return (
 		<article
-			className={`color`}
+			className={`color  ${index > 10 && 'color-light'}`}
 			style={{ backgroundColor: `rgb(${bcg})` }}
 		>
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hex}</p>
+      <p className="color-value">{hexValue}</p>
     </article>
 	);
 };
