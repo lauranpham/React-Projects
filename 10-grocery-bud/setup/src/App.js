e2ea789d42c65358b2ab985c540b4b8bfc868229
Laura Pham <l.pham2311@gmail.com>
2022-03-25 09:55:40 +1100

    grocery bud project: use localStorage item set to persist data

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+	const list = localStorage.getItem('list');
+	return list ? JSON.parse(localStorage.getItem('list')) : [];
+};
 function App() {
 	const [name, setName] = useState('');
-	const [list, setList] = useState([]);
+	const [list, setList] = useState(getLocalStorage());
 	const [isEditing, setIsEditing] = useState(false);
 	const [editId, setEditId] = useState(null);
 	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
@@ -56,6 +60,11 @@ function App() {
 		setEditId(id);
 		setName(specificItem.title);
 	};
+
+	useEffect(() => {
+		localStorage.setItem('list', JSON.stringify(list));
+	}, [list]);
+
 	return (
 		<section className='section-center'>
 			<form action='' className='grocery-form' onSubmit={handleSubmit}>
