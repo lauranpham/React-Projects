f1510865f3d87082f28e29e05500cedea41d7c66
Laura Pham <l.pham2311@gmail.com>
2022-03-24 23:14:05 +1100

    grocery bud project: set static page components

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,36 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName ] = useState('');
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  } 
+
+  const clearItem = () => {
+    console.log('clear')
+  }
+  return (
+    <section className="section-center">
+      <form action="" className="grocery-form" onSubmit={handleSubmit}></form>
+      {alert.show && <Alert/>}
+      <h3>grocery bug</h3>
+      <div className="form-control">
+        <input type="text" className="grocery" placeholder="eggs" value={name} onChange={(e) => setName(e.target.value)} />
+        <button className="submit-btn" type="submit">
+          {isEditing ? 'edit' : 'submit'}
+        </button>
+      </div>
+      <div className="grocery-container">
+        <List/>
+        <button className="clear-btn" onClick={() => clearItem()}>clear items</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
