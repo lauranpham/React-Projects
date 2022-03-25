d12f3f05883dba99ee8fc6c459469389e376edeb
Laura Pham <l.pham2311@gmail.com>
2022-03-25 22:27:45 +1100

    navbar project: add navbar pages and show toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showNav, setShowNav] = useState(false);
+  // use conditional classes to allow for css transitions as opposed to conditional display which would mount/unmount the navbar
+  
+	return (
+		<nav>
+			<div className='nav-center'>
+				<div className='nav-header'>
+					<img src={logo} alt='logo' />
+					<button className='nav-toggle' onClick={()=> setShowNav(!showNav)}>
+						<FaBars />
+					</button>
+				</div>
+				<div className={`${showNav && 'show-container'} links-container`}>
+					<ul className='links'>
+            {links.map((item) => {
+              const {id, url, text} = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+					</ul>
+				</div>
+				<ul className='social-icons'>
+					{social.map((item) => {
+            const {id, url, icon} = item
+						return (
+							<li key={id}>
+								<a href={url}>{icon}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
