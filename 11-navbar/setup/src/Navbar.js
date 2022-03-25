07d6982f810f25f16a1f0f0c60284d556d74b7e9
Laura Pham <l.pham2311@gmail.com>
2022-03-25 22:42:19 +1100

    navbar project: useRef to set dynamic navbar height based on links

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,20 +4,35 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
-  const [showNav, setShowNav] = useState(false);
-  // use conditional classes to allow for css transitions as opposed to conditional display which would mount/unmount the navbar
-  
+  const [showLinks, setShowLinks] = useState(false);
+  // three ways to toggle navbar
+  // 1. conditional display which would mount/unmount the navbar
+  // 2. use conditional classes to allow for css transitions
+  // 3, useRefs on navbar links container and nav links to set dynamic height
+  // useRefs for dynamic height adjustments
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    // check height of links to adjust links container height dynamically
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showLinks])
 	return (
 		<nav>
 			<div className='nav-center'>
 				<div className='nav-header'>
 					<img src={logo} alt='logo' />
-					<button className='nav-toggle' onClick={()=> setShowNav(!showNav)}>
+					<button className='nav-toggle' onClick={()=> setShowLinks(!showLinks)}>
 						<FaBars />
 					</button>
 				</div>
-				<div className={`${showNav && 'show-container'} links-container`}>
-					<ul className='links'>
+				<div className='links-container' ref={linksContainerRef}>
+					<ul className='links' ref={linksRef}>
             {links.map((item) => {
               const {id, url, text} = item;
               return (
