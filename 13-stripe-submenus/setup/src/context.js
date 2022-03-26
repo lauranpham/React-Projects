40599bec94a85180369e9f7ac03002f1927ba892
Laura Pham <l.pham2311@gmail.com>
2022-03-27 10:34:47 +1100

    stripe project: setup global context for app

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,35 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+// set all the shared states, setStates and functions in the AppProvider for
+// wrapping around your entire app in index.js
+export const AppProvider = ({ children }) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+	const openSidebar = () => setIsSidebarOpen(true);
+	const closeSidebar = () => setIsSidebarOpen(false);
+	const openSubmenu = () => setIsSubmenuOpen(true);
+	const closeSubmenu = () => setIsSubmenuOpen(false);
+
+	// set shared functions and states in Provider value
+	return (
+		<AppContext.Provider
+			value={{
+				isSubmenuOpen,
+				isSidebarOpen,
+				openSubmenu,
+				closeSubmenu,
+				openSidebar,
+				closeSidebar,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+// set up custom hook function 
+export const useGlobalContext = () => useContext(AppContext);
