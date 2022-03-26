ec817c7de8da88a5c1fa2ae966d1c2e936fcca84
Laura Pham <l.pham2311@gmail.com>
2022-03-26 14:51:10 +1100

    sidebar and modal project: useGlobalContext in Modal and Sidebar components to toggle open

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
-import {useGlobalContext} from './context'
+import { useGlobalContext } from './context';
 const Home = () => {
-  const {openSidebar, openModal} = useGlobalContext();
-  // returns children of AppContext which is accessed via the useContext hook 
-  console.log(data)
+	const { openSidebar, openModal } = useGlobalContext();
+	// returns children of AppContext which is accessed via the useContext hook
 	return (
 		<main>
 			<button className='sidebar-toggle' onClick={openSidebar}>
 				<FaBars />
 			</button>
-			<button className='btn' onClick={openModal}>show modal</button>
+			<button className='btn' onClick={openModal}>
+				show modal
+			</button>
 		</main>
 	);
 };
--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,33 +1,38 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
 
 const AppContext = React.createContext();
 
 // set up shared states, setstates and functions in provider
-const AppProvider = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+const AppProvider = ({ children }) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openSideBar = () => setIsSidebarOpen(true)
-    const closeSideBar = () => setIsSidebarOpen(false)
-    const openModal = () => setIsModalOpen(true)
-    const closeModal = () => setIsModalOpen(false)
-    
-    return <AppContext.Provider value={{
-        isModalOpen,
-        isSidebarOpen,
-        openModal,
-        closeModal,
-        openSideBar,
-        closeSideBar,
-    }}>{children}</AppContext.Provider>
-}
+	const openSidebar = () => setIsSidebarOpen(true);
+	const closeSidebar = () => setIsSidebarOpen(false);
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
+
+	return (
+		<AppContext.Provider
+			value={{
+				isModalOpen,
+				isSidebarOpen,
+				openModal,
+				closeModal,
+				openSidebar,
+				closeSidebar,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};
 
 // custom hook - need to have prefix with 'use'
-// prevents multiple imports of useContext and AppContext in files 
+// prevents multiple imports of useContext and AppContext in files
 // just import useGlobalContext
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+	return useContext(AppContext);
+};
 
-export {AppContext, AppProvider}
-
+export { AppContext, AppProvider };
