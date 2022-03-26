51308988f71516a2e10e77d60c33db3ae88f331a
Laura Pham <l.pham2311@gmail.com>
2022-03-26 14:31:42 +1100

    side modal project: set up useContext hook and shared functions

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { FaBars } from 'react-icons/fa';
-
+import {useGlobalContext} from './context'
 const Home = () => {
+  const data = useGlobalContext();
+  // returns children of AppContext which is accessed via the useContext hook 
+  console.log(data)
 	return (
 		<main>
 			<button className='sidebar-toggle'>
--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,33 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+// set up shared states, setstates and functions in provider
+const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSideBar = () => setIsSidebarOpen(true)
+    const closeSideBar = () => setIsSidebarOpen(false)
+    const openModal = () => setIsModalOpen(true)
+    const closeModal = () => setIsModalOpen(false)
+    
+    return <AppContext.Provider value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        openSideBar,
+        closeSideBar,
+    }}>{children}</AppContext.Provider>
+}
+
+// custom hook - need to have prefix with 'use'
+// prevents multiple imports of useContext and AppContext in files 
+// just import useGlobalContext
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
+
