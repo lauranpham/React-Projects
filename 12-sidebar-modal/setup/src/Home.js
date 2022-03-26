42c9611d508b7f1fb72ad8304e49f8b058a4d6a1
Laura Pham <l.pham2311@gmail.com>
2022-03-26 14:39:31 +1100

    sidebar modal project: use destructuring to extract prop functions from useGlobalContext

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import {useGlobalContext} from './context'
 const Home = () => {
-  const data = useGlobalContext();
+  const {openSidebar, openModal} = useGlobalContext();
   // returns children of AppContext which is accessed via the useContext hook 
   console.log(data)
 	return (
 		<main>
-			<button className='sidebar-toggle'>
+			<button className='sidebar-toggle' onClick={openSidebar}>
 				<FaBars />
 			</button>
-			<button className='btn'>show modal</button>
+			<button className='btn' onClick={openModal}>show modal</button>
 		</main>
 	);
 };
