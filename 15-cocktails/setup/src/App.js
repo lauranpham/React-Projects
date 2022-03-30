ac2adb4d8db6a4610a98eec2f148126671f36574
Laura Pham <l.pham2311@gmail.com>
2022-03-30 20:11:52 +1100

    cocktail project: use Link from react-router-dom for links in the NavBar component

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -14,6 +14,7 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route exact path="/">
           <Home/>
@@ -25,6 +26,7 @@ function App() {
           <SingleCocktail/>
         </Route>
         <Route path="*">
+          {/* * - regex of all other paths */}
           <Error/>
         </Route>
       </Switch>
