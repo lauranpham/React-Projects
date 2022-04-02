aaf684be4e344be0606a12c920c6ed61ebe2a3eb
Laura Pham <l.pham2311@gmail.com>
2022-04-02 21:11:15 +1100

    cocktails project: set up states and fetch api to populate cocktails

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -5,7 +5,45 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a')
+  const [cocktails, setCocktails] = useState([]);
+  
+  const fetchDrinks = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`${url}${searchTerm}`);
+      const data = await res.json();
+      const {drinks} = data;
+      if (!drinks) {
+        setCocktails([])
+      } else {
+        // map over api array and construct a new array to simplify cocktail fields
+        const newCocktails = drinks.map((drink) =>{
+          const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = drink;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass
+          }
+        })
+        setCocktails(newCocktails);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error)
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm])
+
+  return <AppContext.Provider value={{
+    loading, cocktails, setSearchTerm
+  }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
