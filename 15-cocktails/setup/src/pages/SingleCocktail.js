a420dfdb14697059e5f8c654340c4600a374fbdc
Laura Pham <l.pham2311@gmail.com>
2022-04-03 12:35:00 +1000

    cocktails project: useEffect and async await fetch to extract cocktail details

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -1,15 +1,70 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React from 'react';
+import Loading from '../components/Loading';
+import { useParams, Link } from 'react-router-dom';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
 const SingleCocktail = () => {
-  const {id} = useParams();
-  return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
-  )
-}
+	const { id } = useParams();
+	// keep singleCocktail page as an independent component without use of global context
+	const [loading, setLoading] = React.useState(true);
+	const [cocktail, setCocktail] = React.useState(null);
 
-export default SingleCocktail
+	const getCocktail = async () => {
+		try {
+			const res = await fetch(`${url}${id}`);
+			const data = await res.json();
+			if (data.drinks) {
+				const {
+					strDrink: name,
+					strDrinkThumb: image,
+					strAlcoholic: info,
+					strCategory: category,
+					strGlass: glass,
+					strInstructions: instructions,
+					strIngredient1,
+					strIngredient2,
+					strIngredient3,
+					strIngredient4,
+					strIngredient5,
+				} = data.drinks[0];
+				const ingredients = [
+					strIngredient1,
+					strIngredient2,
+					strIngredient3,
+					strIngredient4,
+					strIngredient5,
+				];
+        const newCocktail = {
+          name, image, info, category, glass, instructions, ingredients
+        }
+				setCocktail(newCocktail);
+			} else {
+				setCocktail(null);
+			}
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
+	};
+	React.useEffect(() => {
+		setLoading(true);
+		getCocktail();
+	}, [id]);
+
+  if (loading) {
+    return <Loading/>
+  }
+  if (!cocktail) {
+    return <h2 className="section-title">no cocktail to display</h2>
+  }
+
+  const {name, image, category, info, glass, instructions, ingredients} = cocktail;
+ 	return (
+		<section>
+      <h2 className="section-title">{name}</h2>
+    </section>
+	);
+};
+
+export default SingleCocktail;
