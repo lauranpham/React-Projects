d0b58c9fdf2d1da4a57bbf71f07cc13762592328
Laura Pham <l.pham2311@gmail.com>
2022-04-03 12:13:01 +1000

    cocktails project: set up search component and search cocktail function

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -2,10 +2,23 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext();
+  const searchValue = React.useRef('');
+  
+  // call setSearchTerm when search value is changed therefore triggering an updated cocktails list
+  const searchCocktails = () =>{
+    setSearchTerm(searchValue.current.value)
+    // must get current.value when using useRef
+  }
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form">
+        <div className="form control">
+          <label htmlFor="name">search your favourite cocktail</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchCocktails} />
+        </div>
+      </form>
+    </section>
   )
 }
 
